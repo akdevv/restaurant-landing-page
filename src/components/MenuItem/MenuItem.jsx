d48185cb4b408e3201ebcb04247e22d63d3acc9a
Ashish Kumar <akdev5520@gmail.com>
2024-02-11 14:46:39 +0530

    implemented menu section

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,7 +1,33 @@
 import "./MenuItem.css";
 
-function MenuItem() {
-	return <div>MenuItem</div>;
+function MenuItem({ title, price, tags }) {
+	return (
+		<div className="app__menuItem">
+			<div className="app__menuItem-head">
+				{/* item name */}
+				<div className="app__menuItem-name">
+					<p className="p__cormorant" style={{ color: "#DCCA87" }}>
+						{title}
+					</p>
+				</div>
+
+				{/* seprator dash */}
+				<div className="app__menuItem-dash" />
+
+				{/* item price */}
+				<div className="app__menuItem-price">
+					<p className="p__cormorant">{price}</p>
+				</div>
+			</div>
+
+			{/* item sub tags */}
+			<div className="app__menuItem-sub">
+				<p className="p__opensans" style={{ color: "#AAA" }}>
+					{tags}
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default MenuItem;
